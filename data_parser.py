940f76304f7a91bfb6ddbd7b67ea22393516a992
Erush01 <erushglr@gmail.com>
2024-03-10 22:55:13 +0300

    parser-updated

--- data_parser.py
+++ data_parser.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import numpy as np
 from constants import DATASET_FOLDER
 
+
 def data_splitter(satellite):
     data_array=[]
     track_numbers=[]
@@ -16,7 +17,11 @@ def data_splitter(satellite):
         for i in lines:
             data_array.append(i.split(' '))
             track_numbers.append(i.split(' ')[9].replace('\n',''))
+
     track_numbers=set(track_numbers)
+    all_data=[[]*x for x in range(len(track_numbers))]
+    labels=[[]*x for x in range(len(track_numbers))]
+
     for data in data_array:
         for idx,track in enumerate(track_numbers):
             if track==data[9].replace('\n',''):
@@ -24,7 +29,9 @@ def data_splitter(satellite):
 
             labels[idx]=class_name
 
+
     return labels,all_data
+
 
 def data_plotter(magnitude_array):
 
@@ -37,9 +44,26 @@ def data_plotter(magnitude_array):
     plt.tight_layout()
     plt.show()
 
+def dataset_creator(satellite_datas):
+    dataset=list()
+    labels=list()
+    for idx,satellite in enumerate(satellite_datas):
+        file_path=os.path.join(DATA_FOLDER,satellite)
+        temp_label,temp_data=data_splitter(file_path)
+        dataset.append(temp_data)
+        labels.append(temp_label)
+        if idx==10:
+            break
+    
+    return labels,dataset
+
 if __name__=='__main__':
     DATA_FOLDER=DATASET_FOLDER
     satellite_datas=os.listdir(DATA_FOLDER)
-    file_path=os.path.join(DATA_FOLDER,satellite_datas[0])
-    data=data_splitter(file_path)
-    data_plotter(data)
+
+    dataset_creator(satellite_datas)
+    # dataset=list()
+    # labels=list()
+    # file_path=os.path.join(DATA_FOLDER,satellite_datas[7])
+    # labels,dataset=data_splitter(file_path)
+
